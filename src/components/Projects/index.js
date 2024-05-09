220f7b1e843e313511ed9e9fa2c947066881f36a
Pramod R <81947919+pramodparam@users.noreply.github.com>
2024-05-09 22:19:55 +0530

    Update index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -26,12 +26,6 @@ const Projects = ({openModal,setOpenModal}) => {
             :
             <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
           }
-          {/* <Divider />
-          {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-            :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-          } */}
           <Divider />
           {toggle === 'machine learning' ?
             <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>

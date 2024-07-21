4e790e9d964bff7a1ceaaf16cb48388d66783d60
Acer <pramodparam24@gmail.com>
2024-07-21 20:39:13 +0530

    resume updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -24,7 +24,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/pramodparam",
   resume:
-    "https://drive.google.com/file/d/1NOX5EPUIBB7Kee39GACKfQdylqLOgOGj/view?usp=sharing",
+    "https://drive.google.com/file/d/1S2PKJoUbX56KFlM8t-Dai_RZ7uH2sekb/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/pramod-r-3078b6200/",
   insta: "https://www.instagram.com/pramod.r_/?igsh=bzgwZ3l6d3dyYm8z",
 };
